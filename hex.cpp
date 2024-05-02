2d953a7cfbe4ac8a285541c6a67b17fcfa188a33
oxzh <gabriel_alcantara12@outlook.com>
2024-05-01 21:16:26 -0300

    update

---
+++ hex.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    long long number = 13091817134234;
+    int letter = 'slowayy';
+    cout << "número é " << setbase(16) << letter << endl;
+}
