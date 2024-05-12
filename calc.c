f73b640c359bcd1ee076ad35c121d2a1e6517e47
Gabriel <gabriel_alcantara12@outlook.com>
2024-05-11 23:12:20 -0300

    c
    created a function who sum two integer!

--- calc.c
+++ calc.c
@@ -3,22 +3,25 @@
 
 int main(){
 
-printf("Calculadora em C!");
-printf("Digite SOMA para soma");
-    char OPRC[4];
-    scanf(OPRC);qte
-
- if (OPRC == SOMA)
+printf("Calculadora em C!\n");
+printf("Digite 10 para soma\n");
+    int OPRC;
+    scanf("%d", &OPRC);
+ if (OPRC == 10)
     {
+        printf("Digite um numero a ser somado.\n");
         int num1;
+        scanf("%d", &num1);
+        printf("Digite outro numero a ser somado.\n");
         int num2;
+        scanf("%d", &num2);
         int res = num1 + num2;
      printf("O resultado é %d.\n", res);
     }
  else
     {
      printf("OPRC, inválida.");
-    };
+    }
     Sleep(500);
 
-};
+}
