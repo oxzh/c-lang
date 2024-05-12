7306b2a907d2b65e8011a5890b2195975b6e3860
Gabriel <gabriel_alcantara12@outlook.com>
2024-05-11 22:34:56 -0300

    c
    creating a calculator in C lang!

--- calc.c
+++ calc.c
@@ -1,1 +1,24 @@
 #include <stdio.h>
+#include <windows.h>
+
+int main(){
+
+printf("Calculadora em C!");
+printf("Digite SOMA para soma");
+    char OPRC[4];
+    scanf(OPRC);qte
+
+ if (OPRC == SOMA)
+    {
+        int num1;
+        int num2;
+        int res = num1 + num2;
+     printf("O resultado é %d.\n", res);
+    }
+ else
+    {
+     printf("OPRC, inválida.");
+    };
+    Sleep(500);
+
+};
