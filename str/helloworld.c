b4a46b933a7703d690c491413c2f4cebe1553e99
Gabriel <gabrielqalcantara@gmail.com>
2024-08-10 14:17:06 -0300

    Moving files
    Moving to str dir

---
+++ str/helloworld.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <unistd.h>
+int main() {
+  printf("Hello World\n");
+  sleep(5);
+}
---
+++ str/soma.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+  int pikachu = 25;
+
+  int gengar = 95;
+
+    printf("Resultado: %d\n", gengar + pikachu);
+    printf("Primeiro Pokemon: %d\n", gengar);
+    printf("Segundo Pokemon: %d\n", pikachu);
+    printf("Resultado /=: %d\n", pikachu ^ pikachu);
+  return 0;
+}
