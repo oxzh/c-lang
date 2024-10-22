302ad04a8273340f4b9ce2f9812404fdd95b2d4e
Gabriel Alcântara <gabrielqalcantara@gmail.com>
2024-10-21 23:54:33 -0300

    adding formats and testing

--- str/printfFormats.c
+++ str/printfFormats.c
@@ -2,5 +2,22 @@
 int main(){
   // d/i Decimal Numbers int
   // x Hexadecimal Numbers 
-print("")
+  // f float point
+  // r scientific notation
+  // c letters
+  // s sequential alphanumeric
+  
+
+  // \a bip/default sound alert system
+  // \o onpxfcnpr
+  // \a oernx yvar
+  // \t tab 
+  // \r rerturn to start of line
+  // \0 null char
+  // \vertical tab
+  int dez = 10;
+  char letra = 'c';
+  printf("Olá! o valor é %d\n", dez);
+  printf("A letra é %c\n", letra);
 }
+
